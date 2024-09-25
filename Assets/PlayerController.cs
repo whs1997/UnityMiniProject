8ad30452bd65e09a3e9df7973835e402eb4e4d8e
whs1997 <whs1997@naver.com>
2024-09-25 18:09:24 +0900

    [FEAT] 플레이어 이동 구현하기
    이동 로직 내용 추가

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -5,17 +5,26 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public enum State { Idle, Jump };
+
     [SerializeField] Rigidbody2D rigid;
     [SerializeField] SpriteRenderer render;
-    private float x;
+    [SerializeField] Collider2D coll;
+    private State state;
 
-    [SerializeField] bool isGround;
-
-    [SerializeField] float jumpPower;
     [SerializeField] float moveSpeed;
 
+    [SerializeField] float minJumpPower;
+    [SerializeField] float maxJumpPower;
+    [SerializeField] float jumpCharge;
+    [SerializeField] float jumpPower;
+    [SerializeField] bool isGround;
+    [SerializeField] bool isCharging;
 
-    
+    [SerializeField] Transform groundCheck;
+    [SerializeField] LayerMask groundLayer;
+
+    private float x;
 
     private void Start()
     {
@@ -24,21 +33,19 @@ public class PlayerController : MonoBehaviour
     private void Update()
     {
         x = Input.GetAxisRaw("Horizontal");
-
-        // Move();
-        // Jump();
+        Move();
+        Jump();        
+        GroundChecker();
     }
 
-    private void StartMove()
+    private void Move()
     {
-    }
+        if(isGround && !isCharging && rigid.velocity.y == 0)
+        {
+            float moveDir = Input.GetAxisRaw("Horizontal");
+            rigid.velocity = new Vector2(moveDir * moveSpeed, rigid.velocity.y);
+        }
 
-    private void Jump()
-    {
-    }
-
-    private void Flip()
-    {
         if (x < 0)
         {
             render.flipX = true; // 왼쪽으로 입력하면 좌우 반전
@@ -49,4 +56,39 @@ public class PlayerController : MonoBehaviour
         }
     }
 
+    private void Jump()
+    {
+        // isGround = Physics2D.Raycast(groundCheck.position, Vector2.down, 0.2f, groundLayer);
+
+        if (Input.GetKey(KeyCode.Space) && isGround)
+        {
+            // charging
+            rigid.velocity = new Vector2(0, 0); // 이동하지 않음
+            isCharging = true; // 점프 충전중엔 이동할 수 없음
+            jumpPower += jumpCharge * Time.deltaTime; // 점프힘 증가
+            jumpPower = Mathf.Clamp(jumpPower, minJumpPower, maxJumpPower); // 최대 점프힘까지만
+        }
+
+        if (Input.GetKeyUp(KeyCode.Space) && isGround)
+        {
+            float jumpDirection = x;
+            rigid.velocity = new Vector2(jumpDirection * moveSpeed, jumpPower);
+            isGround = false;
+            isCharging = false; // 충전 해제
+            jumpPower = 0f; // 점프힘 0 초기화
+        }
+    }
+
+    private void GroundChecker()
+    {
+        isGround = Physics2D.Raycast(groundCheck.position, Vector2.down, 0.1f, groundLayer);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGround = true;
+        }
+    }
 }
